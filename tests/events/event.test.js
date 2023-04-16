ad9a890bf7fed63d1cdb4a28fc7e846b98324e09
Josiah360 <josiaholajide@gmail.com>
2023-04-16 12:03:14 +0100

    pass test

--- tests/events/event.test.js
+++ tests/events/event.test.js
@@ -1,7 +1,7 @@
 const { Event, getTagLine } = require('../../js/events/event')
 
-test("return Sold Out tag when no tickets are left", () => {
-    const event = new Event(1, 'Summer BBQ', 40, 100, 0)
+test('returns Event Sold Out when there are no tickets left', () => {
+    const event = new Event(1, 'Ilashe Beach', 40, 200, 0)
     const tagLine = getTagLine(event, 10, true)
 
     expect(tagLine).toBe('Event Sold Out!')
