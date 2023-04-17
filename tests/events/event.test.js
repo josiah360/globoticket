ebc976fe786706e5b44e3293daa7eacf1d561156
Josiah360 <josiaholajide@gmail.com>
2023-04-17 17:20:52 +0100

    Finish tests for basket

--- tests/events/event.test.js
+++ tests/events/event.test.js
@@ -14,5 +14,19 @@ describe('getTagLine', () => {
 
         expect(tagLine).toBe('Hurry only 7 tickets left!')
     })
+
+    test('returns if event is popular', () => {
+        const event = new Event(3, 'Made In Lagos', 80, 100, 12)
+        const tagLine = getTagLine(event, 10, true)
+
+        expect(tagLine).toBe("This Event is getting a lot of interest. Don't miss out, purchase your ticket now!")
+    })
+
+    test('returns if event is not popular', () => {
+        const event = new Event(3, 'Made In Lagos', 80, 100, 12)
+        const tagLine = getTagLine(event, 10, false)
+
+        expect(tagLine).toBe("Don't miss out, purchase your ticket now!")
+    })
 })
 
