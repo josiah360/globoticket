a234e1d5c8f5f111f4126a13f09394946895dd85
Josiah360 <josiaholajide@gmail.com>
2023-04-17 16:13:49 +0100

    revert event and basket

--- tests/events/event.test.js
+++
@@ -1,8 +0,0 @@
-const { Event, getTagLine } = require('../../js/events/event')
-
-test('returns Event Sold Out when there are no tickets left', () => {
-    const event = new Event(1, 'Ilashe Beach', 40, 200, 0)
-    const tagLine = getTagLine(event, 10, true)
-
-    expect(tagLine).toBe('Event Sold Out!')
-})
