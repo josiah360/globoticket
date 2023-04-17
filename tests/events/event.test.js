08a950630b6b6738b062daf4d7620f3a5555af21
Josiah360 <josiaholajide@gmail.com>
2023-04-17 17:04:23 +0100

    More testing for tagLine function

---
+++ tests/events/event.test.js
@@ -0,0 +1,18 @@
+const { Event, getTagLine } = require('../../js/events/event')
+
+describe('getTagLine', () => {
+    test('returns Event Sold Out when there are no tickets left', () => {
+        const event = new Event(1, 'Elegushi Beach', 40, 100, 0)
+        const tagLine = getTagLine(event, 10, true)
+    
+        expect(tagLine).toBe('Event Sold Out!')
+    })
+
+    test('returns amount of tickets left', () => {
+        const event = new Event(2, 'Davido Show', 50, 100, 7)
+        const tagLine = getTagLine(event, 10, true)
+
+        expect(tagLine).toBe('Hurry only 7 tickets left!')
+    })
+})
+
