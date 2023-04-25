e6031cf73b93c9aa3b6e5c26f58ccc97ec5e082e
Josiah360 <josiaholajide@gmail.com>
2023-04-25 16:50:33 +0100

    Test promotions using mock

--- tests/promotions/promotions.test.js
+++ tests/promotions/promotions.test.js
@@ -6,7 +6,17 @@ describe('generateReferralCode', () => {
 
         const referralCode = generateReferralCode(userId)
 
-        // expect(referralCode).toContain(userId)
+        expect(referralCode).toContain(userId)
         expect(referralCode).toMatch(/^#FRIEND-#\d{3}-#\d{4}$/)
+    })
+
+    test("returns correct referral code", () => {
+        const randomMock = jest.spyOn(global.Math, 'random').mockReturnValue(12345)
+
+        const referralCode = generateReferralCode('678')
+
+        expect(referralCode).toBe('#FRIEND-#345-#678')
+
+        expect(randomMock).toHaveBeenCalled()
     })
 })
