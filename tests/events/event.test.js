d9b4d97eba91e28877b7f3e073f535fcc4535007
Josiah360 <josiaholajide@gmail.com>
2023-04-27 20:35:56 +0100

    Tested eventjs

--- tests/events/event.test.js
+++ tests/events/event.test.js
@@ -9,12 +9,28 @@ describe("getTagLine", () => {
         expect(tagLine).toBe('Event Sold Out!', 10, true)
     })
 
-    test("Tests, if remaining ticket less than minimum ticket count", () => {
+    test("Tests if remaining ticket is less than minimum ticket count", () => {
         const event = new Event(1, "Timeless Concert", 30.00, 100, 8)
 
         const tagLine = getTagLine(event,10, true)
 
         expect(tagLine).toBe("Hurry only 8 tickets left!")
+    })
+
+    test("Tests if remaining ticket is greater than minimum ticket count and is popular", () => {
+        const event = new Event(1, "Timeless Concert", 30.00, 100, 15)
+
+        const tagLine = getTagLine(event,10, true)
+
+        expect(tagLine).toBe("This Event is getting a lot of interest. Don't miss out, purchase your ticket now!")
+    })
+
+    test("Tests if remaining ticket i greater than minimum ticket count but not popular", () => {
+        const event = new Event(1, "Timeless Concert", 30.00, 100, 15)
+
+        const tagLine = getTagLine(event,10, false)
+
+        expect(tagLine).toBe("Don't miss out, purchase your ticket now!")
     })
 })
 
