3d7503f2f364675212985c3f1456d2fd2016597b
Josiah360 <josiaholajide@gmail.com>
2023-04-27 22:43:57 +0100

    Test serialized to json

--- tests/basket/basket.test.js
+++ tests/basket/basket.test.js
@@ -55,3 +55,54 @@ describe("showAdverts", () => {
         expect(basket.showAdverts(user)).toBe(false)
     })
 })
+
+describe("serializeBasketToJson", () => {
+    const event1 = new Event(1, "Timeless Concert", 39.00, 100, 46)  
+    const event2 = new Event(2, "Made In Lagos", 42.00, 90, 32)
+    const event3 = new Event(3, "Burna Live", 52.00, 75, 16)
+
+    const items = [
+        new BasketItem(event1, 5),
+        new BasketItem(event2, 2),
+        new BasketItem(event3, 3)
+    ]
+
+    serializedItems = basket.serializeBasketItemsToJson(items)
+
+    expect(serializedItems).toEqual([
+        {
+            event: {
+                id: 1,
+                name: "Timeless Concert",
+                ticketPrice: 39.00,
+                totalTickets: 100,
+                ticketsRemaining: 46,
+                date: undefined
+            },
+            ticketCount: 5
+        },
+        {
+            event: {
+                id: 2,
+                name: "Made In Lagos",
+                ticketPrice: 42.00,
+                totalTickets: 90,
+                ticketsRemaining: 32,
+                date: undefined
+            },
+            ticketCount: 2
+        },
+        {
+           event: {
+            id: 3,
+            name: "Burna Live",
+            ticketPrice: 52.00,
+            totalTickets: 75,
+            ticketsRemaining: 16,
+            date: undefined
+           },
+           ticketCount: 3
+        }
+    ])
+
+})
