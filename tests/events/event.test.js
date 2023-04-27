4556cda93f3c76e7e4cdf3882b76a04edb0dc8a2
Josiah360 <josiaholajide@gmail.com>
2023-04-27 20:56:06 +0100

    Testing createEvent

--- tests/events/event.test.js
+++ tests/events/event.test.js
@@ -1,4 +1,5 @@
-const { Event, getTagLine } = require("../../js/events/event")
+const { InvalidEventNameError } = require("../../js/error-handling/exceptions")
+const { Event, getTagLine, createEvent } = require("../../js/events/event")
 
 describe("getTagLine", () => {
     test("Returns Event Sold Out if there are no tickets left", () => {
@@ -31,6 +32,23 @@ describe("getTagLine", () => {
         const tagLine = getTagLine(event,10, false)
 
         expect(tagLine).toBe("Don't miss out, purchase your ticket now!")
+    })
+})
+
+describe("createEvent", () => {
+    test("Throws error if name is a string and length is greater than 200", () => {
+        
+        expect(() => createEvent(100, 10, 60)).toThrow(new InvalidEventNameError("Event name cannot exceed 200 characters"))
+    })
+
+    test("Throws error if price is not a number and price is less than 0", () => {
+        
+        expect(() => createEvent("Timeless", "10", 60)).toThrow(new InvalidEventNameError("Event price must be more or equal to 0"))
+    })
+
+    test("Throws error if price is not a number and available tickets is less than 1", () => {
+        
+        expect(() => createEvent("Timeless", 2, 0)).toThrow(new InvalidEventNameError("Event tickets must be more than 0"))
     })
 })
 
