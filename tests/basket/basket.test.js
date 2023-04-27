9ab1f16d915a6cad736dcb33d5d091a4f2af4c41
Josiah360 <josiaholajide@gmail.com>
2023-04-27 21:25:43 +0100

    Test basket

--- tests/basket/basket.test.js
+++ tests/basket/basket.test.js
@@ -1,39 +1,57 @@
 const basket = require("../../js/basket/basket");
 const { Event } = require("../../js/events/event");
 const { BasketItem } = require("../../js/basket/basketitem");
+const { User } = require("../../js/users/users")
 let events = [];
 let items = [];
 
-test("Test calculates total basket price when no discount applied", () => {
-    events = [
-        new Event(1, "A Night At The Proms", 2500.00, 2500, 2500),
-        new Event(2, "Taylor Swift", 50.00, 5500, 2500),
-        new Event(3, "Rage Against The Machine", 35.00, 2500, 2500),
-    ];
+describe("calculateTotal", () => {
+    test("Test calculates total basket price when no discount applied", () => {
+        events = [
+            new Event(1, "A Night At The Proms", 2500.00, 2500, 2500),
+            new Event(2, "Taylor Swift", 50.00, 5500, 2500),
+            new Event(3, "Rage Against The Machine", 35.00, 2500, 2500),
+        ];
+    
+        items = [
+            new BasketItem(events[0], 1),
+            new BasketItem(events[1], 4),
+            new BasketItem(events[2], 2),
+        ];
+        const total = basket.calculateTotal(items);
+    
+        expect(total).toBeCloseTo(2770.00, 2)
+    });
+    
+    test("Test calculates total basket price with discount", () => {
+        events = [
+            new Event(1, "A Night At The Proms", 2500.00, 2500, 2500),
+            new Event(2, "Taylor Swift", 50.00, 5500, 2500),
+            new Event(3, "Rage Against The Machine", 35.00, 2500, 2500),
+        ];
+    
+        items = [
+            new BasketItem(events[0], 1),
+            new BasketItem(events[1], 4),
+            new BasketItem(events[2], 2),
+        ];
+        const total = basket.calculateTotal(items, 800);
+    
+        expect(total).toBeCloseTo(1970.00, 2)
+    });
+})
 
-    items = [
-        new BasketItem(events[0], 1),
-        new BasketItem(events[1], 4),
-        new BasketItem(events[2], 2),
-    ];
-    const total = basket.calculateTotal(items);
+describe("showAdverts", () => {
+    test("returns truthy if user is not premium", () => {
+        const user = new User(1, 'Josiah')
 
-    expect(total).toBeCloseTo(2770.00, 2)
-});
+        expect(basket.showAdverts(user)).toBe(true)
+    })
 
-test("Test calculates total basket price with discount", () => {
-    events = [
-        new Event(1, "A Night At The Proms", 2500.00, 2500, 2500),
-        new Event(2, "Taylor Swift", 50.00, 5500, 2500),
-        new Event(3, "Rage Against The Machine", 35.00, 2500, 2500),
-    ];
+    test("returns falsy if user is premium", () => {
+        const user = new User(1, 'Josiah')
+        user.isPremium = true
 
-    items = [
-        new BasketItem(events[0], 1),
-        new BasketItem(events[1], 4),
-        new BasketItem(events[2], 2),
-    ];
-    const total = basket.calculateTotal(items, 800);
-
-    expect(total).toBeCloseTo(1970.00, 2)
-});
+        expect(basket.showAdverts(user)).toBe(false)
+    })
+})
