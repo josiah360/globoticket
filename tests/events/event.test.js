37df76452532e2e1eaba75e6184e8f0af2cb099b
Josiah360 <josiaholajide@gmail.com>
2023-04-27 20:27:24 +0100

    Partialy tested getTagLine

--- tests/events/event.test.js
+++ tests/events/event.test.js
@@ -1,9 +1,20 @@
 const { Event, getTagLine } = require("../../js/events/event")
 
-test("Returns Event Sold Out if there are no tickets left", () => {
-    const event = new Event(1, "Timeless Concert", 40.00, 100, 80)
+describe("getTagLine", () => {
+    test("Returns Event Sold Out if there are no tickets left", () => {
+        const event = new Event(1, "Timeless Concert", 40.00, 100, 0)
+    
+        const tagLine = getTagLine(event)
+    
+        expect(tagLine).toBe('Event Sold Out!', 10, true)
+    })
 
-    const tagLine = getTagLine(event)
+    test("Tests, if remaining ticket less than minimum ticket count", () => {
+        const event = new Event(1, "Timeless Concert", 30.00, 100, 8)
 
-    expect(tagLine).toBe('Event Sold Out!')
+        const tagLine = getTagLine(event,10, true)
+
+        expect(tagLine).toBe("Hurry only 8 tickets left!")
+    })
 })
+
