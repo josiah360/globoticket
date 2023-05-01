38e59fb8bddcafd7e15e9c9e1e628ff720270074
Josiah360 <josiaholajide@gmail.com>
2023-05-01 17:52:52 +0100

    Resolve a bug

--- js/users/account/purchaseHistory/__mocks__/purchaseHistory.js
+++ js/users/account/purchaseHistory/__mocks__/purchaseHistory.js
@@ -2,25 +2,27 @@ const purchaseHistory = jest.createMockFromModule("../purchaseHistory")
 
 function __getPurchaseHistory() {
     const response = {
-        readyState: 444,
+        readyState: 4,
         onreadystatechange: null,
-        events: [
-            {
-                name: "Punk Goes Pop - 90s",
-                tickets: 2,
-                price: 40.00,
-            },
-            {
-                name: "Adventures Live!",
-                tickets: 5,
-                price: 120.00,       
-            },
-            {
-                name: "Folk dance party!",
-                tickets: 3,
-                price: 75.00,
-            }
-        ]
+        response: {
+            events: [
+                {
+                    name: "Punk Goes Pop - 90s",
+                    tickets: 2,
+                    price: 40.00,
+                },
+                {
+                    name: "Adventures Live!",
+                    tickets: 5,
+                    price: 120.00,       
+                },
+                {
+                    name: "Folk dance party!",
+                    tickets: 3,
+                    price: 75.00,
+                }
+            ]
+        }
     }
 
     return response
--- tests/users/account2.test.js
+++ tests/users/account2.test.js
@@ -4,24 +4,25 @@ jest.mock('../../js/users/account/purchaseHistory/purchaseHistory')
 
 describe("getPastPurchases", () => {
     test("returns purchased events if ready state is 4", () => {
-        const userId = '1234'
+        const userId = '123'
 
         expect(getPastPurchases(userId)).toEqual([
-            {
-                name: "Punk Goes Pop - 90s",
-                tickets: 2,
-                price: 40.00,
-            },
-            {
-                name: "Adventures Live!",
-                tickets: 5,
-                price: 120.00,       
-            },
-            {
-                name: "Folk dance party!",
-                tickets: 3,
-                price: 75.00,
-            }
-        ])
+                {
+                    name: "Punk Goes Pop - 90s",
+                    tickets: 2,
+                    price: 40.00,
+                },
+                {
+                    name: "Adventures Live!",
+                    tickets: 5,
+                    price: 120.00,       
+                },
+                {
+                    name: "Folk dance party!",
+                    tickets: 3,
+                    price: 75.00,
+                }
+            ]
+        )
     })
 })
